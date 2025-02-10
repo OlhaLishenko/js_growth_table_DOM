4c050cd3a574351409cbae017397ed09b77fbb4b
Olha Lishenko <lishenko.olha@gmail.com>
2025-02-10 15:32:00 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,71 @@
 'use strict';
 
-// write code here
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+const table = document.querySelector('.field');
+const row = document.querySelectorAll('tr');
+
+let rowCounter = 4;
+let columnCounter = 4;
+
+appendRow.addEventListener('click', () => {
+  if (rowCounter === 10) {
+    appendRow.setAttribute('disabled', true);
+
+    return;
+  }
+
+  const newRow = row.cloneNode(true);
+
+  rowCounter++;
+
+  table.insertAdjacentElement('beforeend', newRow);
+});
+
+removeRow.addEventListener('click', () => {
+  if (rowCounter === 2) {
+    removeRow.setAttribute('disabled', true);
+
+    return;
+  }
+
+  const deletedRow = table.rows[table.rows.length - 1];
+
+  rowCounter--;
+
+  deletedRow.remove();
+});
+
+appendColumn.addEventListener('click', () => {
+  if (columnCounter === 10) {
+    appendColumn.setAttribute('disabled', true);
+
+    return;
+  }
+
+  columnCounter++;
+
+  row.forEach((line) => {
+    const newColumn = line.lastElementChild.cloneNode(true);
+
+    line.insertAdjacentElement('beforeend', newColumn);
+  });
+});
+
+removeColumn.addEventListener('click', () => {
+  if (columnCounter === 2) {
+    removeColumn.setAttribute('disabled', true);
+
+    return;
+  }
+
+  columnCounter--;
+
+  row.forEach((line) => {
+    const deletedColumn = line.lastElementChild;
+
+    deletedColumn.remove();
+  });
+});
